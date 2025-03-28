728165edaf9a729626fc695d3d43249c15ee732d
SawanPanwar <skpanwar25@gmail.com>
2025-03-28 17:49:11 +0530

    28-03-2025

---
+++ angular-demo/src/app/login/login.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+
+  message = ''
+
+  loginId = ''
+
+  password = ''
+
+  constructor(private route : Router){
+
+  }
+
+  signIn() {
+    console.log('in sign in method')
+
+    if (this.loginId == 'admin' && this.password == 'admin') {
+      console.log('Login ID: ', this.loginId)
+      console.log('Password: ', this.password)
+      this.route.navigateByUrl('welcome')
+    } else {
+      this.message = 'login & password invalid'
+    }
+  }
+}
